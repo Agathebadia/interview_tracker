f36d2dadf19ec8154501bbd8a5b04e35b40db0b7
Susanne Werner-Most <susanne.werner.most@gmail.com>
2019-11-26 17:33:35 +0100

    solve merge conflict

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -5,20 +5,36 @@ class JobApplicationsController < ApplicationController
     #authorize @application
   end
 
+
   def create
     @job_application = JobApplication.new(job_application_params)
     #authorize @application
 
     @job_application.user_id = current_user.id
-    if @application.save
+    if @job_application.save
       redirect_to :home #@application redirect to different page later
     else
       render :new #
     end
   end
+
+  def edit
+    @job_application = JobApplication.find
+  end
+
+  def update
+    @job_application = JobApplication.find(params[:id])
+    @job_application.update(job_application_params)
+    if @job_application.save!
+      redirect_to @job_application
+    else
+      render :edit
+    end
+  end
+
   private
 
   def job_application_params
-    params.require(:job_application).permit(:job_title, :company_name, :recruiter_name, :recruiter_email)
+    params.require(:job_application).permit(:job_title, :company_name, :recruiter_name, :recruiter_email, :favorites)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, only: [:show]
   root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :job_applications, only: [:edit, :update]
+  resources :profiles, only: [:show]
 end
