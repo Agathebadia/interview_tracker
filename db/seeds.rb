09d2a1ae61f0473ace142adcc92d45a1ce393e1a
Agathe Badia <ab@honeypot.io>
2019-11-26 17:03:12 +0100

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 JobApplication.destroy_all
-
+puts "Destroyed job app"
+User.destroy_all
+puts "Destroyed users"
 
 user = User.new(email: "anna@example.com", password: "123456")
 user.save!
 puts "User was created"
 
-job_opening = JobApplication.new(job_title: "Developer", company_name: "Zalando", recruiter_email: "azerty@example.com", status: "Interviewing", recruiter_name: "Sarah")
+job_opening1 = JobApplication.new(job_title: "Developer", company_name: "Zalando", recruiter_email: "azerty@example.com", status: "Interviewing", recruiter_name: "Sarah")
 
-job_opening.user_id = user.id
-puts "We are adding user to job_opening"
+job_opening2 = JobApplication.new(job_title: "Developer", company_name: "Zalando", recruiter_email: "azerty@example.com", status: "Interviewing", recruiter_name: "Sarah")
 
-job_opening.save!
-job.opening
-puts "Job opening was created"
+job_opening3 = JobApplication.new(job_title: "Developer", company_name: "Zalando", recruiter_email: "azerty@example.com", status: "Interviewing", recruiter_name: "Sarah")
+
+job_opening1.user_id = user.id
+puts "We are adding user to job_opening1"
+
+job_opening2.user_id = user.id
+puts "We are adding user to job_opening2"
+
+job_opening3.user_id = user.id
+puts "We are adding user to job_opening3"
+
+job_opening1.save!
+job_opening2.save!
+job_opening3.save!
+
+puts "Finished"
 
 
