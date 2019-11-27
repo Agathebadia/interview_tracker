629f087dc8db7f29ee482329df06b0dd28b21c87
Agathe Badia <ab@honeypot.io>
2019-11-27 15:50:21 +0100

    Flatpickr installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
-import "flatpickr/"
+import "flatpickr/dist/flatpickr.min.css"
+require("flatpickr/dist/themes/material_green.css");
+
 
 flatpickr(".datepicker", {
   enableTime: true,
