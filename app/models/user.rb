a5e9c3398c3826d83934e197c6c36ac12b4eeecf
Leonardo Costa <leo.costa.programming@gmail.com>
2019-11-29 11:51:32 +0100

    Added has_one :profile to user.rb + created path - @user.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :job_applications
-
+  has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
