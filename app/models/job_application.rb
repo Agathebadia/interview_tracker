7fc352b78a2c0fa50d22a7bceffe02b8f18c172d
Agathe Badia <ab@honeypot.io>
2019-11-30 09:28:55 +0100

    first steps of the favorite feature!

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -8,4 +8,14 @@ class JobApplication < ApplicationRecord
   validates :company_name, presence: true
   validates :recruiter_email, presence: true
 
+  def favorite!
+  self.favorite = true
+  self.save!
 end
+
+def unfavorite!
+  self.favorite = false
+  self.save!
+end
+end
+
