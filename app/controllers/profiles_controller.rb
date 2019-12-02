83bb0c1dec297dddde8da512580c84e4d0ed9e7a
Agathe Badia <ab@honeypot.io>
2019-12-02 11:05:34 +0100

    filter is workiiiiing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,23 @@
 class ProfilesController < ApplicationController
+
+
   def show
     @job_applications = find_applications
-
+    if params[:status] == "Applied"
+      @job_applications = JobApplication.where(status: "Applied")
+    elsif params[:status] == "Interviewing"
+      @job_applications = JobApplication.where(status: "Interviewing")
+    elsif params[:status] == "Technical assignment"
+      @job_applications = JobApplication.where(status: "Technical assignment")
+    elsif params[:status] == "Management interview"
+      @job_applications = JobApplication.where(status: "Management interview")
+    elsif params[:status] == "Offer"
+      @job_applications = JobApplication.where(status: "Offer")
+    elsif params[:status] == "Hired"
+      @job_applications = JobApplication.where(status: "Hired")
+    elsif params[:status] == "Closed"
+      @job_applications = JobApplication.where(status: "Closed")
+    end
   end
 
   private
