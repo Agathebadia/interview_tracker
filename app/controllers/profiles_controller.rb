ec96658e06c1b979766d87148b458a23bbc06032
Leonardo Costa <leo.costa.programming@gmail.com>
2019-12-03 18:39:24 +0100

    changing forms according to new style

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,24 @@
 # in show is the Dashboard also could be romoved to job application
 class ProfilesController < ApplicationController
+  def show
+    # @job_applications = JobApplication.left_outer_joins(:interviews).order("interviews.date ASC")
+    # @job_applications = JobApplication.includes(:interviews).order("interviews.date ASC")
+    @job_applications = current_user.job_applications
+    @job_applications = @job_applications.status(status) if status
+    @job_applications = @job_applications.favorites if favorite
 
- def new
+    upcoming_range = Time.zone.now..(1.week.from_now)
+    with_interviews = @job_applications.joins(:interviews)
+    @upcoming_interviews = with_interviews.where("interviews.date" => upcoming_range)
+    @with_interviews = with_interviews.where("interviews.date > ?", 1.week.from_now).where.not(id: @upcoming_interviews)
+
+    @no_interviews = @job_applications.left_outer_joins(:interviews).where(interviews: { job_application: nil })
+  end
+
+  def new
     @profile = Profile.new
     #@first_name = first_name.new
     #@last_name = last_name.new
-
   end
 
   def create
@@ -30,30 +43,9 @@ class ProfilesController < ApplicationController
     redirect_to profile_path(current_user)
   end
 
-
-
-
-
-
-
-
-
 # Controller for the Dashboard
 
-  def show
-    # @job_applications = JobApplication.left_outer_joins(:interviews).order("interviews.date ASC")
-    # @job_applications = JobApplication.includes(:interviews).order("interviews.date ASC")
-    @job_applications = current_user.job_applications
-    @job_applications = @job_applications.status(status) if status
-    @job_applications = @job_applications.favorites if favorite
 
-    upcoming_range = Time.zone.now..(1.week.from_now)
-    with_interviews = @job_applications.joins(:interviews)
-    @upcoming_interviews = with_interviews.where("interviews.date" => upcoming_range)
-    @with_interviews = with_interviews.where("interviews.date > ?", 1.week.from_now).where.not(id: @upcoming_interviews)
-
-    @no_interviews = @job_applications.left_outer_joins(:interviews).where(interviews: { job_application: nil })
-  end
 
   private
 
